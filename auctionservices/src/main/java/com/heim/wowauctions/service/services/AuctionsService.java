55fb0faf8022cc4d8e4a2e10c2eaf4c8543c8fee
sbenner <sergey.benner@gmail.com>
2017-10-25 01:18:48 +0300

    fixes for web iface

--- auctionservices/src/main/java/com/heim/wowauctions/service/services/AuctionsService.java
+++ auctionservices/src/main/java/com/heim/wowauctions/service/services/AuctionsService.java
@@ -76,7 +76,7 @@ public class AuctionsService {
         String localurl= String.format(tooltipUrl,id);
         String out = cache.get(id);
         if(StringUtils.isEmpty(out)) {
-            out=httpReqHandler.getData(localurl);
+            out=httpReqHandler.getData(localurl).replaceAll("[\n\t\r]", "");
             cache.put(id,out.trim());
         }
         return out;
